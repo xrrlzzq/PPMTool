94b94d0112187c330fec2648ef93a6bf79fb8c39
xrrlzzq <35709196+xrrlzzq@users.noreply.github.com>
2019-08-04 14:56:45 -0400

    add project model

--- PPMToolFullStack/src/main/java/io/rongxie/ppmtool/PpmtoolApplication.java
+++ PPMToolFullStack/src/main/java/io/rongxie/ppmtool/PpmtoolApplication.java
@@ -2,8 +2,10 @@ package io.rongxie.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+//@EntityScan(basePackages = { "domain" })
 public class PpmtoolApplication {
 
 	public static void main(String[] args) {
---
+++ PPMToolFullStack/src/main/java/io/rongxie/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package io.rongxie.ppmtool.repositories;
+
+import io.rongxie.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+}
