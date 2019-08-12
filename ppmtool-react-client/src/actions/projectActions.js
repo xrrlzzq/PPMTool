4e79f0826f6bc187b3977fa1c946f3437497b300
xrrlzzq <35709196+xrrlzzq@users.noreply.github.com>
2019-08-12 19:03:22 -0400

    Create project from react

---
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { GET_ERRORS } from "./types";
+
+export const createProject = (project, history) => async dispatch => {
+  try {
+    const res = await axios.post("http://localhost:8090/api/project", project);
+    history.pushState("/dashboard");
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 class AddProject extends Component {
   constructor() {
     super();
@@ -29,7 +31,7 @@ class AddProject extends Component {
       start_date: this.state.start_date,
       end_date: this.state.end_date
     };
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
   render() {
     return (
@@ -108,5 +110,10 @@ class AddProject extends Component {
     );
   }
 }
-
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired
+};
+export default connect(
+  null,
+  { createProject }
+)(AddProject);
