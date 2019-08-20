0a1c02cd002ee9031bfd7802538a179dc6a896eb
xrrlzzq <35709196+xrrlzzq@users.noreply.github.com>
2019-08-20 17:43:11 -0400

    Get project by id

--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-
-export default class UpdateProject extends Component {
+import { getProject } from "../../actions/projectActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+class UpdateProject extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
+  }
   render() {
     return (
       <div className="project">
@@ -63,3 +70,15 @@ export default class UpdateProject extends Component {
     );
   }
 }
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project.project
+});
+export default connect(
+  mapStateToProps,
+  { getProject }
+)(UpdateProject);
